b5476102fbaf0182d74f57a36a3851adbb613c0e
jihanizzathul <105841104021@student.unismuh.ac.id>
2024-08-16 13:23:45 +0800

    chapter 3

--- integrasiMultimedia2.py
+++ integrasiMultimedia2.py
@@ -1,21 +1,33 @@
-import tkinter as tk  
+import tkinter as tk
 from tkinter import filedialog
 from pydub import AudioSegment
-from pydub.playback import play
-
+import subprocess
+import tempfile
+import os
 
 root = tk.Tk()
 root.title("Multimedia Application")
+
 
 def play_music():
     file_path = filedialog.askopenfilename()
     if file_path:
         audio = AudioSegment.from_file(file_path)
-        play(audio)
 
-# Membuat tombol untuk memutar musik
+        
+        with tempfile.NamedTemporaryFile(suffix=".mp3", delete=False) as temp_audio_file:
+            temp_file_path = temp_audio_file.name
+            audio.export(temp_file_path, format="mp3")
+        
+        
+        subprocess.call(['ffplay', '-nodisp', '-autoexit', temp_file_path])
+
+        
+        os.remove(temp_file_path)
+
+
 play_button = tk.Button(root, text="Play", command=play_music)
 play_button.pack()
 
-# Menjalankan loop acara Tkinter
+
 root.mainloop()
